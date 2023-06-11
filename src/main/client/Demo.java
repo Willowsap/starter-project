1f73cb61310f90fd0d710c28bbbb311f8d14c3ee
Willow Sapphire <willowsapphire@protonmail.com>
2023-06-11 15:55:36 -0400

    added src folder

---
+++ src/main/client/Demo.java
@@ -0,0 +1,20 @@
+package client;
+
+/**
+ * Demo for the program.
+ * 
+ * @author
+ * @version
+ */
+public class Demo
+{
+    /**
+     * Runs the demo.
+     * 
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+
+    }
+}
